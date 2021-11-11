ec284aa3ae2ec5f4a284b50727748bb3f72bbbad
GP RAJ <gpraj@outlook.in>
2021-11-12 00:55:46 +0530

    home is ready and profile layout ready

--- assets/js/style-script.js
+++ assets/js/style-script.js
@@ -1,25 +1,71 @@
 //homeContent-buttons
+ 
+let btnWeb = document.querySelector(".btnWeb");
+let btnGraph = document.querySelector(".btnGraph");
 
-function homeContentButtons(){
-  let btnWeb = document.querySelector(".homeContent-buttons__btnWeb");
-  let btnGraph = document.querySelector(".homeContent-buttons__btnGraph");
-  btnGraph.addEventListener("mouseover", function(){
-    btnGraph.style.backgroundColor = "rgb(225, 0, 81)";
-    btnWeb.style.backgroundColor = "rgba(225, 0, 81,0.5)";  
-    btnGraph.addEventListener("mouseout", function(){
-      btnWeb.style.backgroundColor = "rgb(225, 0, 81)";
-      btnGraph.style.backgroundColor = "rgba(225, 0, 81,0.5)";
-    });
+btnGraph.addEventListener("mouseover", function(){
+  btnWeb.classList.replace("btnWeb","btnGraph");
+  btnGraph.classList.replace("btnGraph","btnWeb");
+  btnGraph.addEventListener("mouseout", function(){
+    btnGraph.classList.replace("btnWeb","btnGraph");
+    btnWeb.classList.replace("btnGraph","btnWeb");
   });
-} homeContentButtons();
+});
 
-//window-scroll
+//active-navbar
 
 let header = document.querySelector(".header");
 let navLink = document.querySelectorAll(".navLink");
+let linkBar = document.querySelectorAll(".navLink__bar");
 
-for(let i=0; i < navLink.length; i++){
-  navLink[i].addEventListener("click", function(){
-    navLink[i].classList.add("active");
-  });
+for(let keyOne = 0; keyOne < navLink.length; keyOne ++){
+  navLink[keyOne].addEventListener("click", function(){
+    for(let keyTwo = 0; keyTwo < navLink.length; keyTwo ++){
+      if(keyOne < keyTwo){
+        navLink[keyTwo].classList.remove("active");
+        linkBar[keyTwo - 1].style.visibility = "hidden";
+      }
+      else{
+        for(let keyThree = 0; keyThree < keyOne; keyThree ++){
+          linkBar[keyThree].style.visibility = "visible";
+        }
+        navLink[keyTwo].classList.add("active");
+      }
+    }
+  })
 }
+
+//profile-content-scroll
+
+let profile = document.querySelector(".profile-contents");
+profile.addEventListener("scroll", function(){
+
+})
+
+//window-scroll
+
+/*
+
+window.addEventListener("scroll", function(){
+  let scrollPosition = window.scrollY;
+  if(scrollPosition > 250){
+    navLink[0].classList.add("active");    
+    if(scrollPosition > 300){
+      linkBar[0].style.visibility = "visible";
+      if(scrollPosition > 350){
+        navLink[1].classList.add("active");
+      }
+      else{
+        navLink[1].classList.remove("active");
+      }
+    }
+    else{
+      linkBar[0].style.visibility = "hidden";
+    }
+  }
+  else{
+    navLink[0].classList.remove("active");
+  }
+})
+
+*/
